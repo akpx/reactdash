f51077b88663798722e75b6c4de1ccce9ed47584
Abhishek Pathak <c-apathak@stearns.com>
2017-11-16 11:08:01 -0800

    comments in api

--- src/api/api.js
+++ src/api/api.js
@@ -5,7 +5,7 @@ export const getAllApp = () => {
       return axios({
         method:'get',
         url:' https://rink.hockeyapp.net/api/2/apps',
-        headers: {'X-HockeyAppToken': ''},      
+        headers: {'X-HockeyAppToken': 'Enter Hockey App ID here'},      
       })
         .then(function(response) {
         return response.data;
